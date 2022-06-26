423feb15776c21c9fb8e4636d9112a037efa68a3
kartikey pathak <kartikeypathak007@gmail.com>
2022-06-26 16:53:05 +0530

    Chnages in Code

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Text('This is my Default Text!'),
+        body: Text('I commit changes in git'),
       ),
     );
   }
